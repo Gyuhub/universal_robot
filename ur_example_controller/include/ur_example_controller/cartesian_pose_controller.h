b2902ef7a493ea8d864627258a48d8c746c122a4
Gyuhub <alsrb0820@gmail.com>
2022-10-09 19:10:43 +0900

    add a custom package

---
+++ ur_example_controller/include/ur_example_controller/cartesian_pose_controller.h
@@ -0,0 +1,74 @@
+#pragma once
+
+// C++ STL
+#include <iostream>
+#include <vector>
+#include <memory>
+
+// Eigen
+#include "eigen3/Eigen/Dense"
+
+// ROS common
+#include "ros/ros.h"
+
+// ROS control
+#include "controller_interface/controller.h"
+#include "hardware_interface/joint_command_interface.h"
+
+// ROS messages
+#include "sensor_msgs/JointState.h"
+#include "geometry_msgs/PoseStamped.h"
+
+// ROS additional libraries
+#include "kdl_parser/kdl_parser.hpp"
+#include "kdl/chain.hpp"
+#include "kdl/chainjnttojacsolver.hpp"
+#include "kdl/chainfksolverpos_recursive.hpp"
+#include "kdl/chainiksolverpos_nr.hpp"
+#include "kdl/chainiksolvervel_pinv.hpp"
+#include "kdl/frames.hpp"
+#include "kdl/jacobian.hpp"
+#include "kdl/jntarray.hpp"
+#include "pluginlib/class_list_macros.h"
+#include "realtime_tools/realtime_publisher.h"
+#include "tf2_kdl/tf2_kdl.h"
+
+namespace ur_example_controller
+{
+
+class CartesianPoseController : public controller_interface::Controller<hardware_interface::PositionJointInterface>
+{
+public:
+    CartesianPoseController();
+    ~CartesianPoseController();
+
+    bool init(hardware_interface::PositionJointInterface *robot_hw, ros::NodeHandle &nh) override;
+    void starting(const ros::Time &time) override;
+    void update(const ros::Time &time, const ros::Duration &period) override;
+
+private:
+    // publishers and subscribers
+    realtime_tools::RealtimePublisher<geometry_msgs::PoseStamped> ee_state_pub_;
+    ros::Subscriber command_sub_;
+
+    // kdl library configuration
+    KDL::Tree ur5_tree_;
+    KDL::Chain ur5_chain_;
+    std::shared_ptr<KDL::ChainFkSolverPos_recursive> fk_solver_pos_rec_;
+    std::shared_ptr<KDL::ChainIkSolverVel_pinv> ik_solver_vel_pinv_;
+    std::unique_ptr<KDL::ChainIkSolverPos_NR> ik_solver_pos_nr;
+
+    std::shared_ptr<KDL::JntArray> jnt_pos_start_;
+    std::shared_ptr<KDL::Frame> ee_pos_goal_;
+
+    // ros control configuration
+    std::vector<hardware_interface::JointHandle> joint_handle_;
+    int joint_num_;
+    std::string base_frame_id_, ee_frame_id_;
+    geometry_msgs::PoseStamped ee_state_, ee_desired_;
+
+    // callback function for command subscriber
+    void GetDesiredEndEffectorPose(const geometry_msgs::PoseStamped::ConstPtr &msg);
+};
+
+} // namespace ur_example_controller
---
+++ ur_example_controller/src/cartesian_pose_controller.cpp
@@ -0,0 +1,158 @@
+#include "ur_example_controller/cartesian_pose_controller.h"
+#include "pluginlib/class_list_macros.hpp"
+
+namespace ur_example_controller
+{
+CartesianPoseController::CartesianPoseController()
+{
+}
+
+CartesianPoseController::~CartesianPoseController()
+{
+}
+
+bool CartesianPoseController::init(hardware_interface::PositionJointInterface *robot, ros::NodeHandle &nh)
+{
+    std::vector<std::string> joint_names;
+    if (!nh.getParam("joints", joint_names) || joint_names.size() != 6)
+    {
+        ROS_ERROR("Not appropriate joint names are found! Please check the name of the joints again");
+        return false;
+    }
+    joint_num_ = joint_names.size();
+    std::string robot_description;
+    if (!nh.getParam("/robot_description", robot_description))
+    {
+        ROS_ERROR("Failed to get the robot_description!");
+        return false;
+    }
+    for (unsigned int i = 0; i < joint_num_; i++)
+    {
+        try
+        {
+            joint_handle_.push_back(robot->getHandle(joint_names[i]));
+        }
+        catch (const hardware_interface::HardwareInterfaceException &e)
+        {
+            ROS_ERROR_STREAM("Exception thrown: " << e.what());
+            return false;
+        }
+    }
+    if (!nh.getParam("base_frame_id", base_frame_id_))
+    {
+        ROS_ERROR("Failed to get the name of the base!");
+        return false;        
+    }
+    if (!nh.getParam("ee_frame_id", ee_frame_id_))
+    {
+        ROS_ERROR("Failed to get the name of the end-effecotr!");
+        return false;        
+    }
+
+    // get kinematic tree from robot_description
+    if (!kdl_parser::treeFromString(robot_description, ur5_tree_))
+    {
+        ROS_ERROR("Failed to parse the tree from robot_description!");
+        return false;
+    }
+
+    // get kinematic chain from tree
+    if (!ur5_tree_.getChain(base_frame_id_, ee_frame_id_, ur5_chain_))
+    {
+        ROS_ERROR("Failed to get the chain from tree!");
+        return false;
+    }
+
+    // create solvers
+    fk_solver_pos_rec_ = std::make_shared<KDL::ChainFkSolverPos_recursive>(ur5_chain_);
+    ik_solver_vel_pinv_ = std::make_shared<KDL::ChainIkSolverVel_pinv>(ur5_chain_, 0.0001, 1000);
+    ik_solver_pos_nr = std::make_unique<KDL::ChainIkSolverPos_NR>(ur5_chain_, *fk_solver_pos_rec_, *ik_solver_vel_pinv_, 1000);
+
+    // initialize configuration
+    jnt_pos_start_ = std::make_shared<KDL::JntArray>(joint_num_);
+
+    // initialize publishers and subscribers
+    ee_state_pub_.init(nh, "ee_state", 1);
+    ee_state_.header.frame_id = base_frame_id_;
+    ee_state_.header.stamp = ros::Time::now();
+    std::lock_guard<realtime_tools::RealtimePublisher<geometry_msgs::PoseStamped>> lock(ee_state_pub_);
+    command_sub_ = nh.subscribe("/ee_pose_desired", 1, &CartesianPoseController::GetDesiredEndEffectorPose, this);
+
+    ROS_INFO("Successfully initialize the controller!");
+    return true;
+}
+
+void CartesianPoseController::starting(const ros::Time &time)
+{
+    // get initial position of each joint
+    for (int i = 0; i < joint_num_; i++)
+    {
+        jnt_pos_start_->data(i) = joint_handle_[i].getPosition();
+    }
+    ROS_INFO("Initial joint configuration of the UR5: %f %f %f %f %f %f", jnt_pos_start_->data(0),
+                                                                          jnt_pos_start_->data(1),
+                                                                          jnt_pos_start_->data(2),
+                                                                          jnt_pos_start_->data(3),
+                                                                          jnt_pos_start_->data(4),
+                                                                          jnt_pos_start_->data(5));
+    // get initial cartesian position and convert current joint position to end-effector's postion
+    KDL::Frame ee_pos_start;
+    fk_solver_pos_rec_->JntToCart(*jnt_pos_start_, ee_pos_start);
+    ee_pos_goal_ = std::make_shared<KDL::Frame>(ee_pos_start);
+    return;
+}
+
+void CartesianPoseController::update(const ros::Time &time, const ros::Duration &period)
+{
+    // get position of each joint
+    for (int i = 0; i < joint_num_; i++)
+    {
+        jnt_pos_start_->data(i) = joint_handle_[i].getPosition();
+    }
+
+    // convert current joint position to end-effector's postion
+    KDL::Frame ee_pos_start;
+    fk_solver_pos_rec_->JntToCart(*jnt_pos_start_, ee_pos_start);
+
+    // compute inverse kinematics
+    KDL::JntArray jnt_pos_goal(joint_num_);
+    ik_solver_pos_nr->CartToJnt(*jnt_pos_start_, *ee_pos_goal_, jnt_pos_goal);
+
+    // publish states of the robot
+    geometry_msgs::TransformStamped tf_stamped = tf2::kdlToTransform(ee_pos_start);
+    ee_state_.pose.position.x = tf_stamped.transform.translation.x;
+    ee_state_.pose.position.y = tf_stamped.transform.translation.y;
+    ee_state_.pose.position.z = tf_stamped.transform.translation.z;
+    ee_state_.pose.orientation = tf_stamped.transform.rotation;
+    if (ee_state_pub_.trylock())
+    {
+        ee_state_pub_.msg_ = ee_state_;
+        ee_state_pub_.unlockAndPublish();
+    }
+
+    // set joint command
+    for (int i = 0; i < joint_num_; i++)
+    {
+        joint_handle_[i].setCommand(jnt_pos_goal.data(i));
+    }
+    return;
+}
+
+void CartesianPoseController::GetDesiredEndEffectorPose(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
+    ee_desired_ = *msg;
+    geometry_msgs::TransformStamped t;
+    t.header.frame_id = base_frame_id_;
+    t.child_frame_id = ee_frame_id_;
+    t.transform.translation.x = ee_desired_.pose.position.x;
+    t.transform.translation.y = ee_desired_.pose.position.y;
+    t.transform.translation.z = ee_desired_.pose.position.z;
+    t.transform.rotation = ee_desired_.pose.orientation;
+    *ee_pos_goal_ = tf2::transformToKDL(t);
+    return;
+}
+
+
+} // namespace ur_example_controller
+
+PLUGINLIB_EXPORT_CLASS(ur_example_controller::CartesianPoseController, controller_interface::ControllerBase)
